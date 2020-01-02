9145814440b2932bb52643f1b548efd62be94887
87244099 <872440899@qq.com>
2020-01-02 23:48:25 +0800

    first commit

---
+++ src/main/java/com/github/hcsp/io/Main.java
@@ -0,0 +1,26 @@
+package com.github.hcsp.io;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://sina.cn/index/feed?from=touch&Ver=10");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            System.out.println(EntityUtils.toString(entity1));
+        } finally {
+            response1.close();
+        }
+
+    }
+}
