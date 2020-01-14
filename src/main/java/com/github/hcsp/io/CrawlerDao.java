2ca8fa7671631c568ff471a212872004111204b7
wgd <872440899@qq.com>
2020-01-14 21:56:27 +0800

    引入mybatis (#4)
    * 使用mybatis简化sql操作
    
    * 修复CI检测不通过

--- src/main/java/com/github/hcsp/io/CrawlerDao.java
+++ src/main/java/com/github/hcsp/io/CrawlerDao.java
@@ -1,94 +1,23 @@
 package com.github.hcsp.io;
 
-import java.sql.PreparedStatement;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class CrawlerDao implements AutoCloseable{
-    private Connection connection;
-    public CrawlerDao(String file, String username, String password) throws SQLException {
-        connection = DriverManager.getConnection(file, username, password);
-    }
+interface CrawlerDao extends AutoCloseable {
+    String getNextUrl()  throws SQLException;
 
-    public void close() throws SQLException {
-        connection.close();
-    }
+    int deleteUrl(String url) throws SQLException;
 
-    public String getNextUrlThenDelete() throws SQLException {
-        String url = getNextUrl();
-        if (url != null){
-            deleteUrl(connection, url);
-        }
+    List<String> loadUrlsFromDatabase(String sql)  throws SQLException;
 
-        return url;
-    }
-    public  String getNextUrl() throws SQLException {
-        String url = null;
-        String sql = "select * from LINKS_TO_BE_PROCESS limit 1";
-        List<String> urls = loadUrlsFromDatabase(connection, sql);
+    void insertNew(Map<String, String> info)  throws SQLException;
 
-        if (urls.size() > 0){
-            url = urls.get(0);
-        }
+    void insertUrlIntoDatabase(String url, String sql)  throws SQLException;
 
-        return url;
-    }
+    boolean isUrlProcessed(String url)  throws SQLException;
 
-    public int deleteUrl(Connection connection, String url) throws SQLException {
-        String sql = "delete from LINKS_TO_BE_PROCESS where LINK=?";
-        try(PreparedStatement statement = connection.prepareStatement(sql)){
-            statement.setString(1, url);
-            return statement.executeUpdate(); //返回影响的行数
-        }
-    }
+    void insertProcessedUrl(String url)  throws SQLException;
 
-    public List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
-
-        List<String> urls = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement(sql)){
-            try (ResultSet resultSet = statement.executeQuery()){
-                while (resultSet.next()){
-                    urls.add(resultSet.getString(1));
-                }
-            }
-        }
-        return urls;
-    }
-
-    public void insertNew(Map<String, String> info){
-       String sql = "insert into NEWS (title, content, url, created_at, modified_at) values(?,?,?,now(),now())";
-        try(PreparedStatement statement = connection.prepareStatement(sql)){
-            statement.setString(1, info.getOrDefault("title", ""));
-            statement.setString(2, info.getOrDefault("content", ""));
-            statement.setString(3, info.getOrDefault("url", ""));
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public  void insertUrlIntoDatabase(String url, String sql) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement(sql)){
-            statement.setString(1, url);
-            statement.executeUpdate();
-        }
-    }
-
-    public  boolean isUrlProcessed(String url) throws SQLException {
-        String sql = "select * from LINKS_ALREADY_PROCESSED where link=? limit 1";
-        try (PreparedStatement statement = connection.prepareStatement(sql)){
-            statement.setString(1, url);
-            try (ResultSet resultSet = statement.executeQuery()){
-                while (resultSet.next()){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    void insertNewUrl(String url)  throws SQLException;
 }
---
+++ src/main/java/com/github/hcsp/io/MybatisCrawlerDao.java
@@ -0,0 +1,75 @@
+package com.github.hcsp.io;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class MybatisCrawlerDao implements CrawlerDao {
+
+    private SqlSessionFactory sqlSessionFactory;
+
+    MybatisCrawlerDao() throws SQLException, IOException {
+
+        String resource = "db/mybatis/config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    public void close() throws SQLException {
+    }
+
+
+    public  String getNextUrl() throws SQLException {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            return (String) session.selectOne("com.github.hcsp.CrawlerMapper.selectNextUrl");
+        }
+    }
+
+    public int deleteUrl(String url) throws SQLException {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            int row =  session.delete("com.github.hcsp.CrawlerMapper.deleteUrl", url);
+            System.out.println("row="+row);
+            return row;
+        }
+    }
+
+    public List<String> loadUrlsFromDatabase(String sql) throws SQLException {
+        return null;
+    }
+
+    public void insertNew(Map<String, String> info) throws SQLException{
+        String title = info.getOrDefault("title", "");
+        String content = info.getOrDefault("content", "");
+        String url = info.getOrDefault("url", "");
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.insert("com.github.hcsp.CrawlerMapper.insertNew", new News(title, content, url));
+        }
+    }
+
+    public  void insertUrlIntoDatabase(String url, String sql) throws SQLException {
+    }
+
+    public  boolean isUrlProcessed(String url) throws SQLException {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            return session.selectOne("com.github.hcsp.CrawlerMapper.insertProcessedUrl", url) != null;
+        }
+    }
+
+    public void insertProcessedUrl(String url) throws SQLException {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.insert("com.github.hcsp.CrawlerMapper.insertProcessedUrl", url);
+        }
+    }
+    public void insertNewUrl(String url) throws SQLException {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.insert("com.github.hcsp.CrawlerMapper.insertNewUrl", url);
+        }
+    }
+}
